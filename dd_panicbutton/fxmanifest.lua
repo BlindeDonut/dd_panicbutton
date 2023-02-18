5db542612f5d4f6666d4de5a9dfcf7d567e5ae69
BlindeDonut <122988344+BlindeDonut@users.noreply.github.com>
2023-02-19 00:19:13 +0100

    Add files via upload

---
+++ dd_panicbutton/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'BlindeDonut'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+}
+
+server_scripts {
+    'config.lua',
+    'server/main.lua',
+}
+
+escrow_ignore
+{
+	"config.lua"
+}
